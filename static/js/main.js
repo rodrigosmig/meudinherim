64a1bea3a1a905dfa41f9b4d3406c802f602a5bd
Cleano <cleano.ferreira@dce.ufpb.br>
2017-09-28 23:22:48 -0300

    alteracao da pagina inicial

--- static/js/main.js
+++ static/js/main.js
@@ -1,26 +1,37 @@
 
 
-function validar(){
+function validarCadastro(){
     var senha=formCadastrar.tSenha.value;
     var rep_senha=formCadastrar.tConfSenha.value;
+    var nomePessoa=formCadastrar.tNome.value;
+    var emailPessoa=formCadastrar.tMail.value;
     
-        
+    if(nomePessoa==""){
+        alert('Preencha o campo NOME !')
+        formCadastrar.tNome.focus();
+        return false;
+    }
+    
+    if(emailPessoa==""){
+        alert('Preencha o campo E-MAIL !')
+        formCadastar.tMail.focus();
+        return false;
+    }
+    
     if(senha == "" || senha.length<=5){
         alert('Preencha o campo com mínimo 6 caracteres')
         formCadastrar.tSenha.focus();
         return false;
     }
     
-     if(rep_senha == "" || rep_senha.length<=5){
-        alert('Preencha o campo com mínimo 6 caracteres')
-        formCadastrar.tConfSenha.focus();
-        return false;
-    }
+    
     
       if(senha != rep_senha){
         alert('Senhas diferntes')
         formCadastrar.tConfSenha.focus();
         return false;
     }
-    
+  
 }
+
+
