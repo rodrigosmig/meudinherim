061df43ca7f8aff341c9f56e19783a94d5d73ff1
Rodrigo Miguel <rodrigo.santos@dce.ufpb.br>
2017-10-03 20:35:55 -0300

    inclusao da validacao do formulario de gastos

--- principal/views.py
+++ principal/views.py
@@ -12,36 +12,39 @@ def home(request):
 	template = 'principal/home.html'
 	context = {}
 
+	#carrega os lançamentos do banco de dados
+	lancamentos = LancamentosCaixa.objects.all()
+	eventos = []
+
+	#separa os dados que serão utilizados no calendario em um tupla
+	for lancamento in lancamentos:
+		dia = str(lancamento.data.day)
+		if(len(dia) == 1):
+			dia = "0" + dia
+		mes = str(lancamento.data.month)
+		if(len(mes) == 1):
+			mes = "0" + mes
+		ano = str(lancamento.data.year)
+		#concatena a data para o formato do fullcalendar
+		data = ano + "-" + mes + "-" + dia
+		titulo = lancamento.descricao + " : " + str(lancamento.valor) 
+		eventos.append((titulo, data))
+
+	#converte a tupla para o formato json
+	eventos = [{'title': title, 'start': start} for title, start in eventos]
+	eventos = json.dumps(eventos, ensure_ascii=False)
+
+	context['events'] = eventos
+	
+
 	if(request.method == 'POST'):
 		form = LancamentosForm(request.POST)
-		form.save()
-		return HttpResponseRedirect(template)
+		if(form.is_valid()):
+			form.save()
+			return HttpResponseRedirect('/principal/home/')
 	else:
 		form = LancamentosForm()
 
-		#carrega os lançamentos do banco de dados
-		lancamentos = LancamentosCaixa.objects.all()
-		eventos = []
-
-		#separa os dados que serão utilizados no calendario em um tupla
-		for lancamento in lancamentos:
-			dia = str(lancamento.data.day)
-			if(len(dia) == 1):
-				dia = "0" + dia
-			mes = str(lancamento.data.month)
-			if(len(mes) == 1):
-				mes = "0" + mes
-			ano = str(lancamento.data.year)
-			#concatena a data para o formato do fullcalendar
-			data = ano + "-" + mes + "-" + dia
-			titulo = lancamento.descricao + " : " + str(lancamento.valor) 
-			eventos.append((titulo, data))
-
-		#converte a tupla para o formato json
-		eventos = [{'title': title, 'start': start} for title, start in eventos]
-		eventos = json.dumps(eventos, ensure_ascii=False)
-
-		context['events'] = eventos
-		context['form'] = form
-		return render(request, template, context)
-
+		
+	context['form'] = form
+	return render(request, template, context)
