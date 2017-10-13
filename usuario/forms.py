fc472417402c3a47d29bd0fc86435e3e9c03cfdd
Van Eyck <van.silva@dce.ufpb.br>
2017-10-13 17:08:05 -0300

    formulário do cadastro na tela (testem)

--- usuario/forms.py
+++ usuario/forms.py
@@ -1,14 +1,39 @@
 from django import forms 
 from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
-class UsuarioForm(forms.Form):
+# class UsuarioForm(forms.Form):
 
-	login = forms.CharField(label = 'Login', max_length=50)
-	email = forms.EmailField()
-	senha = forms.CharField(label = 'Senha', max_length=50, widget=forms.PasswordInput())
+# 	login = forms.CharField(label = 'Login', max_length=50)
+# 	email = forms.EmailField()
+# 	senha = forms.CharField(label = 'Senha', max_length=50, widget=forms.PasswordInput())
 	
 
-class LoginForm(forms.Form):
+# class LoginForm(forms.Form):
 
-	login = forms.CharField(label = 'Login', max_length=50)
-	senha = forms.CharField(label = 'Senha', max_length=50, widget=forms.PasswordInput())
+# 	login = forms.CharField(label = 'Login', max_length=50)
+# 	senha = forms.CharField(label = 'Senha', max_length=50, widget=forms.PasswordInput())
+
+# adicionando o campo email no formulario do cadastro
+class UsuarioForm(UserCreationForm):
+
+	email = forms.EmailField(label='E-mail')
+
+
+	# sobreescrevendo o save de UserCreationForm
+	def save (self, commit = True):
+
+		user = super(UsuarioForm, self).save(commit=False)
+		user.email = self.cleaned_data['email']
+		if commit:
+			user.save()
+		return user 
+
+	# verifica se tem possui o email (tem que fazer a nível de banco ainda)
+	def clean_email(self):
+		email = self.cleaned_data['email']
+
+		if User.objects.filter(email=email).exists():
+			raise forms.ValidationError('Já existe um usuário com esse e-mail')
+		return email
