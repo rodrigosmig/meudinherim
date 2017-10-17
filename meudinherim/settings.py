4770967e531d79770a7e308f7f4c5f3963c30eb9
Van Eyck <van.silva@dce.ufpb.br>
2017-10-16 23:28:31 -0300

    correcao do login e do cadastro (funcionando)

--- meudinherim/settings.py
+++ meudinherim/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'widget_tweaks',
     'principal',
     'caixa',
     'usuario',
