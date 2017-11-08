04410fd3254818329a8046da23c768b53d7e477f
Cleano <cleano.ferreira@dce.ufpb.br>
2017-11-08 12:54:25 -0300

    infoBox conectando valor

--- metas/views.py
+++ metas/views.py
@@ -1,12 +1,34 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect, HttpResponse, HttpResponseServerError
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect, HttpResponse, HttpResponseServerError
+from banco.models import ContaBanco, LancamentosBanco, SaldoBanco
+from caixa.models import Categoria, SaldoCaixa
+from banco.forms import ContaBancoForm, LancamentosBancoForm
+from django import forms
 
-# Create your views here.
-from django.shortcuts import render
 
 
-# Create your views here.
-
+@login_required
 def metas(request):
+	id_user = request.user.id
 	template='meta/metas.html'
-	return render(request, template)
 
+	agencias = ContaBanco.objects.filter(user_id = id_user)
+	form_agencia = ContaBancoForm()
+
+	contexto = {'formAgencia': form_agencia, 'agencias': agencias}
+
+	
+
+	#busca o saldo de Caixa do usuario e atribui ao contexto
+	saldoC = SaldoCaixa.objects.get(user = request.user)
+	contexto['saldoCaixa'] = saldoC.saldoAtual
+
+	#busca o saldo de Banco do usuario e atribui ao contexto
+	saldoB = SaldoBanco.objects.get(user = request.user)
+	contexto['saldoBanco'] = saldoB.saldoAtual
+
+
+	return render(request, template,contexto)
