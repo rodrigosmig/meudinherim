0966f267779a24ef7e72a160c3191bf1550b5d13
WellingtonLemos <wellington.lourenco@dce.ufpb.br>
2017-11-16 11:04:15 -0300

    Ajustes na Infobox e Visual do Caixa

---
+++ config/forms.py
@@ -0,0 +1,32 @@
+from django.forms import ModelForm
+from django import forms
+from caixa.models import ConfigModal
+
+class ConfigForm(ModelForm):
+    nick = forms.Charfield(
+        label = 'Nick',
+        max_length = 20,
+        required = True,
+        widget = forms.TextInput(
+            attrs = {'class': 'form-control', 'id': 'Digite seu nick'}
+        )
+    )    
+    senha = forms.CharField(
+        label = 'Senha',
+        max_length = 30,
+        required = True,
+        widget = forms.TextInput(
+            attrs = {'class': 'form-control', 'placeholder': 'Digite sua senha'}
+        )
+    )
+    email = forms.Charfield(
+    	label = 'Email',
+    	required = True,
+    	widget = forms.TextInput(
+    			attrs = {'class': 'form-control', 'placeholder': 'Digite seu email'}
+    		)
+    	)
+
+    class Meta:
+        model = ConfigModal
+        fields = ['nick', 'senha', 'email']
