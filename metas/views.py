654ce14edecc72efad784d5e27d4321397fe0048
Cleano <cleano.ferreira@dce.ufpb.br>
2017-11-15 22:14:13 -0300

    alteracao do modelo de metas

--- metas/views.py
+++ metas/views.py
@@ -8,20 +8,19 @@ from caixa.models import Categoria, SaldoCaixa
 from banco.forms import ContaBancoForm, LancamentosBancoForm
 from caixa.forms import LancamentosForm
 from django import forms
-
-
+from metas.forms import CadastrarMetasForm
+from metas.models import Metas
+import json
 
 @login_required
 def metas(request):
 	id_user = request.user.id
 	template='meta/metas.html'
 
-	agencias = ContaBanco.objects.filter(user_id = id_user)
-	form_agencia = ContaBancoForm()
+	contexto = {}
 
-	contexto = {'formAgencia': form_agencia, 'agencias': agencias}
-
-	
+	metas = Metas.objects.filter(user_id = id_user)
+	contexto['formMetas'] = metas
 
 	#busca o saldo de Caixa do usuario e atribui ao contexto
 	saldoC = SaldoCaixa.objects.get(user = request.user)
@@ -32,36 +31,14 @@ def metas(request):
 	contexto['saldoBanco'] = saldoB.saldoAtual
 
 
-	formCaixa = LancamentosForm()
-	#seleciona apenas as categorias do usuario logado
-	formCaixa.fields['categoria'] = forms.ModelChoiceField(
-			queryset = Categoria.objects.filter(user_id = request.user.id),
-			empty_label = 'Nenhum',
-	        widget = forms.Select(
-	            attrs = {'class': 'form-control'}
-	        )
-		)
-	
-	formBanco = LancamentosBancoForm()
-	#Seleciona apenas o banco do usuario para o formulario
-	formBanco.fields['banco'] = forms.ModelChoiceField(
-		queryset = ContaBanco.objects.filter(user_id = request.user.id),
-		empty_label = 'Nenhum',
-        widget = forms.Select(
-            attrs = {'class': 'form-control'}
-        )
-	)
-	#seleciona apenas as categorias do usuario logado
-	formBanco.fields['categoria'] = forms.ModelChoiceField(
-			queryset = Categoria.objects.filter(user_id = request.user.id),
-			empty_label = 'Nenhum',
-	        widget = forms.Select(
-	            attrs = {'class': 'form-control', 'id': 'categoria_banco'}
-	        )
-		)
 
-	#para adicionar lancamento
-	contexto['formLancCaixa'] = formCaixa
-	contexto['formLancBanco'] = formBanco
+	return render(request, template, contexto)
 
-	return render(request, template,contexto)
+
+@login_required
+def cadastroMeta(request):
+
+	pass
+
+
+
