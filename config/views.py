b15333f26eef8f33ff7c47c0270ed6250bd76d76
WellingtonLemos <wellington.lourenco@dce.ufpb.br>
2017-11-18 17:31:03 -0300

    Página de Configuração Editando e Salvando

--- config/views.py
+++ config/views.py
@@ -1,5 +1,41 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import PasswordChangeForm
+from usuario.forms import EditAccountsForm
 
 def config(request):
+    id_user = request.user.id
     template = 'config/config.html'
-    return render(request, template)
+    contexto = {}
+
+    user = User.objects.get(pk = id_user)
+
+    #Editar
+    if(request.method == 'POST'):
+    	form = EditAccountsForm(request.POST, instance = request.user)
+    	if form.is_valid():
+    		form.save()
+    		form = EditAccountsForm(instance = request.user)
+    		contexto['success'] = True
+    	else:
+    		form = EditAccountsForm(instance = request.user)
+
+    form = EditAccountsForm(instance = request.user)
+    contexto = {'formConfig': form }
+
+    return render(request, template, contexto)
+
+    #Editar Senha
+    if(request.method == 'POST'):
+    	form = PasswordChangeForm(request.POST, instance = request.user)
+    	if form.is_valid():
+    		form.save()
+    		form = PasswordChangeForm(instance = request.user)
+    		contexto['success'] = True
+    	else:
+    		form = PasswordChangeForm(instance = request.user)
+
+    form = PasswordChangeForm(instance = request.user)
+    contexto = {'formConfig2': form }
+
+    return render(request, template, contexto)
