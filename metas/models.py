1b12465c21788667dea8b9e6050a646f728b1e69
Rodrigo Miguel <rodrigosmig@gmail.com>
2019-09-30 23:45:44 -0300

    correcao do calculo de metas

--- metas/models.py
+++ metas/models.py
@@ -1,17 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import date
 
 class Metas(models.Model):
 	
-	titulo= models.CharField(max_length = 40)
-	progresso= models.DecimalField(max_digits = 5, decimal_places = 2)
-	valor = models.DecimalField(max_digits = 8, decimal_places = 2)
-	concluida = models.BooleanField(blank = True, default = False)
-	user = models.ForeignKey(User, on_delete = models.CASCADE, default=1)
+	titulo		= models.CharField(max_length = 40)
+	progresso	= models.DecimalField(max_digits = 5, decimal_places = 2)
+	prazo		= models.DateField(default=date.today)
+	valor 		= models.DecimalField(max_digits = 8, decimal_places = 2)
+	concluida 	= models.BooleanField(blank = True, default = False)
+	user 		= models.ForeignKey(User, on_delete = models.CASCADE, default=1)
 
 	def __str__(self):
 		return self.titulo
-	
+
+	class Meta:
+		ordering = ['prazo', 'valor']	
 
 
 
