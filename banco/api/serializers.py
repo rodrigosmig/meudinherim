88a53cf76c84aa9498302cb92763dd0a5c3f5db3
Rodrigo Miguel <rodrigosmig@gmail.com>
2019-12-08 21:57:44 -0300

    endpoint /lancamentos_bancarios

--- banco/api/serializers.py
+++ banco/api/serializers.py
@@ -1,8 +1,24 @@
 from rest_framework import serializers
-from banco.models import ContaBanco
+from banco.models import ContaBanco, LancamentosBanco
 
 class ContaBancoSerializer(serializers.ModelSerializer):
     class Meta:
         model           = ContaBanco
         fields          = ('id', 'banco', 'agencia', 'conta', "tipo", 'dia_fechamento', 'limite', 'saldo', 'user')
         related_fields  = ['user']
+
+class LancamentosBancoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = LancamentosBanco
+        fields = (
+            'id',
+            'banco',
+            'data', 
+            'categoria',
+            'tipo',
+            'descricao', 
+            'valor',
+            'conta_a_pagar', 
+            'conta_a_receber', 
+            'user'
+        )
